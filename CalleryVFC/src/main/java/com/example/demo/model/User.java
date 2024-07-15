592b77e589152fe3c11a6eeb4c66ebb24a29aeb5
Seth Daniel Chritzman <sethchritzman@gmail.com>
2024-07-15 15:25:04 -0400

    Added First and LastName fields to all users.

--- CalleryVFC/src/main/java/com/example/demo/model/User.java
+++ CalleryVFC/src/main/java/com/example/demo/model/User.java
@@ -20,6 +20,8 @@ public class User {
 
     private String username;
     private String password;
+    private String firstName;  
+    private String lastName;
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "users_roles", 
@@ -49,6 +51,23 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 
 	public Set<Role> getRoles() {
